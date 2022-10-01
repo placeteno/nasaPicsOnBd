94d56383e76e0ffacbef18c010f1cebb20ee62e6
Jose Gonzalez <joseraul88@gmail.com>
2022-10-01 12:35:19 -0500

    Photo info is now included in dialog box

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const boton = document.querySelector('.searchBtn');
 const lista = document.querySelector('.imgList');
 const clearBtn = document.querySelector('.clear');
-let ima = [];
+const dialog = document.querySelector('dialog');
+let dataObj = [];
 
 boton.addEventListener('click', function (e) {
   e.preventDefault();
-  const imgs = [];
   const fecha = document.querySelector('input').value;
   const mesDia = fecha.split('-').slice(1).join('-');
   const year = new Date().getFullYear();
@@ -17,6 +17,12 @@ boton.addEventListener('click', function (e) {
     )
       .then(res => res.json())
       .then(data => {
+        dataObj.push({
+          date: data.date,
+          title: data.title,
+          desc: data.explanation,
+          img: data.hdurl,
+        });
         const li = document.createElement('li');
         const img = document.createElement('img');
         li.classList.add('newLi');
@@ -24,17 +30,29 @@ boton.addEventListener('click', function (e) {
         img.src = data.hdurl;
         li.appendChild(img);
         lista.appendChild(li);
-        img.addEventListener('click', () => {
-          console.log('hello');
-        });
       })
       .catch(err => {
         console.log(`error ${err}`);
       });
   }
 });
+lista.addEventListener('click', function (e) {
+  let img = e.target.closest('img');
+  if (!img) return;
+  // if (img.src === dataObj.img) console.log('same');
+  const dialogImg = document.querySelector('dialog>img');
+  dialogImg.src = img.src;
+  dataObj.forEach(obj => {
+    if (obj.img === img.src) {
+      document.querySelector('.dialogTitle').textContent = obj.title;
+      document.querySelector('.dialogDate').textContent = obj.date;
+      document.querySelector('.dialogDesc').textContent = obj.desc;
+    }
+  });
 
-console.log(ima);
+  dialog.showModal();
+});
+
 clearBtn.addEventListener('click', () => {
   lista.innerHTML = '';
 });
