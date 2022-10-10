c250f878be89fc30e88e611d9cb667e153b0dbea
Jose Gonzalez <joseraul88@gmail.com>
2022-10-10 17:03:04 -0500

    Added setTimeout to order pictures

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const searchBtn = document.querySelector('.searchBtn');
+const loading = document.querySelector('.loading-message');
 const photosGrid = document.querySelector('.imgList');
 const clearBtn = document.querySelector('.clearBtn');
 const dialog = document.querySelector('dialog');
@@ -6,17 +7,33 @@ const dialogCloseBtn = document.querySelector('.dialogCloseBtn');
 const imagenes = document.querySelector('.imagenes');
 let dataObj = [];
 
+const getPicture = function (year, mesDia) {
+  return new Promise(function (resolve, reject) {
+    const ok = fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=SbevutgF6LLMeUgl7sZo4XaPLdxsltwAp68fARON&date=${year}-${mesDia}`
+    );
+    setTimeout(() => {
+      if (ok) resolve(ok);
+      else reject(new Error('Problem with NASA API'));
+    }, 5000);
+  });
+};
+
 searchBtn.addEventListener('click', function (e) {
   e.preventDefault();
   const date = document.querySelector('input').value;
   const mesDia = date.split('-').slice(1).join('-');
   const year = new Date().getFullYear();
   photosGrid.innerHTML = '';
+  console.log(loading);
+  loading.style.opacity = 1;
+  loading.innerHTML = 'Loading';
 
   for (let f = 1995; f <= year; f++) {
-    fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=SbevutgF6LLMeUgl7sZo4XaPLdxsltwAp68fARON&date=${f}-${mesDia}`
-    )
+    // fetch(
+    //   `https://api.nasa.gov/planetary/apod?api_key=SbevutgF6LLMeUgl7sZo4XaPLdxsltwAp68fARON&date=${f}-${mesDia}`
+    // )
+    getPicture(f, mesDia)
       .then(res => res.json())
       .then(data => {
         dataObj.push({
@@ -34,6 +51,14 @@ searchBtn.addEventListener('click', function (e) {
         console.log(`error ${err}`);
       });
   }
+  setTimeout(() => {
+    loading.classList.add('done');
+    loading.innerHTML = 'Done!';
+    setTimeout(() => {
+      loading.style.opacity = 0;
+      loading.classList.remove('done');
+    }, 2000);
+  }, 5000);
 });
 
 photosGrid.addEventListener('click', function (e) {
